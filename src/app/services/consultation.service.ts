79d0c83661f6a3b93d19134748769df0c28fa3ca
NipuniNuwanthika <wmnipuninuwanthika@gmail.com>
2022-07-07 00:27:39 +0530

    new update

--- src/app/services/consultation.service.ts
+++ src/app/services/consultation.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -8,10 +8,20 @@ export class ConsultationService {
   baseUrl: string = 'http://localhost:3000/';
   constructor(private http: HttpClient) { }
 
+  generateHedaer() {
+    var token = window.localStorage.getItem('token');
+    const hedaerConfig = {
+      'Content-type': 'application/json',
+      Authorization:
+      `Token ${token}`,
+    };
+    return new HttpHeaders(hedaerConfig);
+  }
+  
   listConsultants(){
     return this.http.get(this.baseUrl+ 'consultants');
   }
   requestConsultation(consultationRequestObj: any, consultantId:any){
-    return this.http.post(this.baseUrl+ 'consultationReq/add/'+consultantId,consultationRequestObj);
+    return this.http.post(this.baseUrl+ 'consultationReq/add/'+consultantId,consultationRequestObj,{headers: this.generateHedaer(),});
   }
 }
